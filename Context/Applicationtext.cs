372f0158e98d9db9f0d1f931aeb234fa7e34fb4a
Olga Bachurina <bachuri12@ukr.net>
2024-02-08 23:45:08 +0200

    Add project files.

---
+++ Context/Applicationtext.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleAppTaskdop1hw0502.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace ConsoleAppTaskdop1hw0502.Context
+{
+    public class Applicationtext : DbContext
+    {
+        public DbSet<Assignment> Assignments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Task0502;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            var methodInfo = typeof(Applicationtext).GetMethod(nameof(CalculateWorkdays), new[] { typeof(DateTime), typeof(DateTime) });
+            // Сопоставление хранимой функции
+            modelBuilder.HasDbFunction(methodInfo)
+       .HasName("CalculateWorkdays") 
+       .HasSchema("dbo"); 
+       
+        }
+
+        //метод для использования хранимой функции
+        public int CalculateWorkdays(DateTime startDate, DateTime endDate)
+            => throw new InvalidOperationException("This method is for use with Entity Framework Core only and has no implementation.");
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleAppTaskdop1hw0502.Context;
+using ConsoleAppTaskdop1hw0502.Models;
+
+namespace ConsoleAppTaskdop1hw0502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    using (var db = new Applicationtext())
+            //    {
+            //        var AssignmentToAdd = new List<Assignment>
+            //{
+            //new Assignment { Name = "Разработка проекта", StartDate = new DateTime(2024, 2, 1), EndDate = new DateTime(2024, 2, 15) },
+            //new Assignment { Name = "Тестирование модулей", StartDate = new DateTime(2024, 2, 16), EndDate = new DateTime(2024, 2, 25) },
+            //new Assignment { Name = "Внедрение системы", StartDate = new DateTime(2024, 3, 1), EndDate = new DateTime(2024, 3, 10) },
+            //new Assignment { Name = "Подготовка документации", StartDate = new DateTime(2024, 3, 11), EndDate = new DateTime(2024, 3, 20) }
+            // };
+
+            //        db.Assignments.AddRange(AssignmentToAdd);
+            //        db.SaveChanges();
+            //    }
+            //}
+            //using (var db = new Applicationtext())
+            //{
+            //    var requiredWorkdays = 7;
+            //    var Assignments = db.Assignments
+            //        .Where(t => db.CalculateWorkdays(t.StartDate, t.EndDate) >= requiredWorkdays)
+            //        .ToList();
+
+            //    foreach (var Assignment in Assignments)
+            //    {
+            //        Console.WriteLine($"Assignment: {Assignment.Name}");
+            //    }
+            //}
+            //using (var db = new Applicationtext())
+            //{
+            //    int minWorkdaysRequired = 5;
+
+            //    // запрос, использующий функцию CalculateWorkdays для фильтрации задач
+            //    var filteredAssignments = db.Assignments
+            //        .Where(a => db.CalculateWorkdays(a.StartDate, a.EndDate) >= minWorkdaysRequired)
+            //        .Select(a => new
+            //        {
+            //            a.Name,
+            //            StartDate = a.StartDate,
+            //            EndDate = a.EndDate,
+            //            Workdays = db.CalculateWorkdays(a.StartDate, a.EndDate)
+            //        })
+            //        .ToList();
+            //    foreach (var assignment in filteredAssignments)
+            //    {
+            //        Console.WriteLine($"Name: {assignment.Name}, StartDate: {assignment.StartDate.ToShortDateString()}, EndDate: {assignment.EndDate.ToShortDateString()}, Workdays: {assignment.Workdays}");
+            //    }
+            //}
+        }
+    }
+}
